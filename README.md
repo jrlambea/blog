67464ee9a85c95cbbe161692037c2ea876c1c9d2
Jose Ramón Lambea <hello@jrlambea.me>
2021-10-11 21:09:37 +0200

    Remove output and configure new theme

--- README.md
+++ README.md
@@ -3,3 +3,25 @@
 ## Notas
 
 Blog personal basado en la tecnología de [Pelican](https://github.com/getpelican/pelican) y la plantilla [Flex](https://github.com/alexandrevicenzi/flex).
+
+## Inicialización
+
+Para hacerlo funcionar en local, se deberán clonar los repositorios de `pelican-themes` (algunos temas como `Flex` están fuera de ese repositorio):
+
+```shell
+git clone --depth=1 https://github.com/getpelican/pelican-themes
+git clone --depth=1 https://github.com/alexandrevicenzi/Flex ./pelican-themes/Flex
+git clone --depth=1 https://github.com/nairobilug/pelican-alchemy/ ./pelican-themes/alchemy
+git clone --depth=1 https://github.com/Pelican-Elegant/elegant ./pelican-themes/elegant
+git clone --depth=1 https://github.com/gfidente/pelican-svbhack ./pelican-themes/svbhack
+git clone --depth=1 https://github.com/petrnohejl/MinimalXY ./pelican-themes/MinimalXY
+git clone --depth=1 https://github.com/molivier/nest ./pelican-themes/nest
+git clone --depth=1 https://github.com/jvanz/pelican-hyde ./pelican-themes/hyde
+
+```
+
+Instalar requisitos de python:
+
+```shell
+pip3 install -r requirements.txt
+```
--- pelicanconf.py
+++ pelicanconf.py
@@ -4,8 +4,8 @@ from __future__ import unicode_literals
 
 AUTHOR = 'J.R. Lambea'
 SITENAME = 'J.R. Blog'
-SITEURL = 'https://jrlambea.me'
-SITESUBTITLE = 'SysAdmin | Security Operations | Cloud Infrastructure'
+SITEURL = '/'
+SITESUBTITLE = u'J.R. Blog'
 
 PATH = 'content'
 
@@ -36,19 +36,97 @@ DEFAULT_PAGINATION = 10
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
-THEME = 'themes/Flex'
+THEME = 'pelican-themes/elegant'
 
-# Flex specific settings
 STATIC_PATHS = ['img']
-PYGMENTS_STYLE = 'monokai'
-THEME_COLOR = 'dark'
-THEME_COLOR_AUTO_DETECT_BROWSER_PREFERENCE = True
-THEME_COLOR_ENABLE_USER_OVERRIDE = True
 
-PYGMENTS_STYLE = 'emacs'
-PYGMENTS_STYLE_DARK = 'monokai'
+PYGMENTS_STYLE = 'native'
 DISQUS_SITENAME = 'blog-ecg5wppj8o'
 
 COPYRIGHT_NAME = 'J. R. Lambea'
 COPYRIGHT_YEAR = '2020'
 GITHUB_CORNER_URL = 'https://github.com/jrlambea/blog/'
+
+# NEST Template
+# Minified CSS
+NEST_CSS_MINIFY = True
+# Add canonical link element to top page header and all article/author/category/tag page header
+NEST_REL_CANONICAL_LINK = True
+# Add items to top menu before pages
+MENUITEMS = [('Homepage', '/'),('Categories','/categories.html')]
+# Add header background image from content/images : 'background.jpg'
+NEST_HEADER_IMAGES = 'photo-1541728472741-03e45a58cf88.jpg'
+NEST_HEADER_LOGO = '/images/logo.png'
+# Footer
+NEST_SITEMAP_COLUMN_TITLE = u'Sitemap'
+NEST_SITEMAP_MENU = [('Archives', '/archives.html'),('Tags','/tags.html'), ('Authors','/authors.html')]
+NEST_SITEMAP_ATOM_LINK = u'Atom Feed'
+NEST_SITEMAP_RSS_LINK = u'RSS Feed'
+NEST_SOCIAL_COLUMN_TITLE = u'Social'
+NEST_LINKS_COLUMN_TITLE = u'Links'
+NEST_COPYRIGHT = u'&copy; J.R. Lambea 2020'
+# Footer optional
+NEST_FOOTER_HTML = ''
+# index.html
+NEST_INDEX_HEAD_TITLE = u'Homepage'
+NEST_INDEX_HEADER_TITLE = u'J.R. Blog'
+NEST_INDEX_HEADER_SUBTITLE = u'System Administration | Security Operations | Cloud Infrastructure'
+NEST_INDEX_CONTENT_TITLE = u'Last Posts'
+# archives.html
+NEST_ARCHIVES_HEAD_TITLE = u'Archives'
+NEST_ARCHIVES_HEAD_DESCRIPTION = u'Posts Archives'
+NEST_ARCHIVES_HEADER_TITLE = u'Archives'
+NEST_ARCHIVES_HEADER_SUBTITLE = u'Archives for all posts'
+NEST_ARCHIVES_CONTENT_TITLE = u'Archives'
+# article.html
+NEST_ARTICLE_HEADER_BY = u'By'
+NEST_ARTICLE_HEADER_MODIFIED = u'modified'
+NEST_ARTICLE_HEADER_IN = u'in category'
+# author.html
+NEST_AUTHOR_HEAD_TITLE = u'Posts by'
+NEST_AUTHOR_HEAD_DESCRIPTION = u'Posts by'
+NEST_AUTHOR_HEADER_SUBTITLE = u'Posts archives'
+NEST_AUTHOR_CONTENT_TITLE = u'Posts'
+# authors.html
+NEST_AUTHORS_HEAD_TITLE = u'Author list'
+NEST_AUTHORS_HEAD_DESCRIPTION = u'Author list'
+NEST_AUTHORS_HEADER_TITLE = u'Author list'
+NEST_AUTHORS_HEADER_SUBTITLE = u'Archives listed by author'
+# categories.html
+NEST_CATEGORIES_HEAD_TITLE = u'Categories'
+NEST_CATEGORIES_HEAD_DESCRIPTION = u'Archives listed by category'
+NEST_CATEGORIES_HEADER_TITLE = u'Categories'
+NEST_CATEGORIES_HEADER_SUBTITLE = u'Archives listed by category'
+# category.html
+NEST_CATEGORY_HEAD_TITLE = u'Category Archive'
+NEST_CATEGORY_HEAD_DESCRIPTION = u'Category Archive'
+NEST_CATEGORY_HEADER_TITLE = u'Category'
+NEST_CATEGORY_HEADER_SUBTITLE = u'Category Archive'
+# pagination.html
+NEST_PAGINATION_PREVIOUS = u'Previous'
+NEST_PAGINATION_NEXT = u'Next'
+# period_archives.html
+NEST_PERIOD_ARCHIVES_HEAD_TITLE = u'Archives for'
+NEST_PERIOD_ARCHIVES_HEAD_DESCRIPTION = u'Archives for'
+NEST_PERIOD_ARCHIVES_HEADER_TITLE = u'Archives'
+NEST_PERIOD_ARCHIVES_HEADER_SUBTITLE = u'Archives for'
+NEST_PERIOD_ARCHIVES_CONTENT_TITLE = u'Archives for'
+# tag.html
+NEST_TAG_HEAD_TITLE = u'Tag archives'
+NEST_TAG_HEAD_DESCRIPTION = u'Tag archives'
+NEST_TAG_HEADER_TITLE = u'Tag'
+NEST_TAG_HEADER_SUBTITLE = u'Tag archives'
+# tags.html
+NEST_TAGS_HEAD_TITLE = u'Tags'
+NEST_TAGS_HEAD_DESCRIPTION = u'Tags List'
+NEST_TAGS_HEADER_TITLE = u'Tags'
+NEST_TAGS_HEADER_SUBTITLE = u'Tags List'
+NEST_TAGS_CONTENT_TITLE = u'Tags List'
+NEST_TAGS_CONTENT_LIST = u'tagged'
+# Static files
+STATIC_PATHS = ['images', 'extra/robots.txt', 'extra/favicon.ico', 'extra/logo.svg']
+EXTRA_PATH_METADATA = {
+    'extra/robots.txt': {'path': 'robots.txt'},
+    'extra/favicon.ico': {'path': 'favicon.ico'},
+    'extra/logo.svg': {'path': 'logo.svg'}
+}
